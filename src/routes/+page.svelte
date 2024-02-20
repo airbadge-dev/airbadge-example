9cf1e4efe8669851bd130ca29bcf34ee9db5ac9c
Josh Nussbaum <joshnuss@gmail.com>
2024-02-20 17:49:49 +0800

    Adds session data and links to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-   to read the documentation
-</p>
+<script>
+  export let data
+</script>
+
+<h1>AirBadge Example App</h1>
+
+<a href="/pricing">Pricing</a>
+
+<pre>session = {JSON.stringify(data.session, null, 2)}</pre>
+
+{#if data.session}
+  <a href="/billing/portal">Billing portal</a>
+{/if}
