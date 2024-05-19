2a7d90d6d8dc8b7c08641d84063207e73cea5c64
Josh Nussbaum <joshnuss@gmail.com>
2024-05-18 20:58:16 -0400

    Bump to @airbadge/sveltekit v0.0.7

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,16 +3,13 @@ import * as Sentry from '@sentry/sveltekit'
 import { SvelteKitAuth } from '@airbadge/sveltekit'
 
 // use GitHub OAuth provider
-import GitHub from '@auth/core/providers/github'
+import GitHub from '@auth/sveltekit/providers/github'
 
 // use Prisma database adapter
 import { PrismaAdapter } from '@auth/prisma-adapter'
 
 // import Prisma client for database adapter
 import { PrismaClient } from '@prisma/client'
-
-// import env vars for OAuth client
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private'
 
 Sentry.init({
   dsn: 'https://675b8cd6559a5a4806ea76b1d32ff1d3@o4506863366832128.ingest.us.sentry.io/4506863375220736',
@@ -25,17 +22,14 @@ const db = new PrismaClient()
 
 // add Auth.js + Stripe handler
 // API is similar to Auth.js
+const { handle: handleAuth } = SvelteKitAuth({
+  adapter: PrismaAdapter(db),
+  providers: [ GitHub ]
+})
+
 export const handle = sequence(
   Sentry.sentryHandle(),
-  SvelteKitAuth({
-    adapter: PrismaAdapter(db),
-    providers: [
-      GitHub({
-        clientId: GITHUB_ID,
-        clientSecret: GITHUB_SECRET
-      })
-    ],
-
-  })
+  handleAuth,
 )
+
 export const handleError = Sentry.handleErrorWithSentry()
