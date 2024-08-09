40c125a1d36e4b455b3fef887f92178bba73356e
Josh Nussbaum <joshnuss@gmail.com>
2024-08-09 06:01:11 -0400

    Hides portal link when user doesn't have a subscription

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,10 @@
 <h1>AirBadge Example App</h1>
 
 {#if data.session}
-  <a href="/billing/portal">Billing portal</a>
+  {#if data.session.subscription}
+    <a href="/billing/portal">Billing portal</a>
+  {/if}
+
   <a href="/auth/signout">Signout</a>
 {:else}
   <a href="/pricing">Pricing</a>
